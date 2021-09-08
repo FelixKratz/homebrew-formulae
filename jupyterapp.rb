a6160ba3318a5db5c6fe63ec1a8970ea5577f579
FelixKratz <felix.kratz@tu-dortmund.de>
2021-09-08 11:24:00 +0200

    added jupyterlab app

---
+++ jupyterapp.rb
@@ -0,0 +1,12 @@
+class Jupyterapp < Cask
+  name "JupyterApp"
+  desc "A simple standalone macOS Jupyter App"
+  homepage "https://github.com/FelixKratz/JupyterApp-mac"
+  url "https://github.com/FelixKratz/JupyterApp-mac/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "d5ff604267a6ad6465651830b2eb66647f8665dbc01355ab21881eff255c9084"
+
+  def install
+    system "make"
+    app.install "#{buildpath}/Products/Applications/JupyterApp.app"
+  end
+end

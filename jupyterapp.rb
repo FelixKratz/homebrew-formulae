86219d1bf3a0c934c336dfad1415c44821131147
FelixKratz <felix.kratz@tu-dortmund.de>
2021-09-08 11:42:29 +0200

    caskify

--- jupyterapp.rb
+++ jupyterapp.rb
@@ -1,11 +1,14 @@
-class Jupyterapp < Formula
+cask "jupyterapp" do
+  name "JupyterApp"
   desc "A simple standalone macOS Jupyter App"
   homepage "https://github.com/FelixKratz/JupyterApp-mac"
   url "https://github.com/FelixKratz/JupyterApp-mac/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "d5ff604267a6ad6465651830b2eb66647f8665dbc01355ab21881eff255c9084"
 
-  def install
+  preflight do
     system "make"
-    app.install "#{buildpath}/Products/Applications/JupyterApp.app"
+  end
+
+  app "#{buildpath}/Products/Applications/JupyterApp.app"
   end
 end

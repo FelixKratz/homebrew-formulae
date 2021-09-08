60239f99da4bbd87cf3d1a52f022d31841be9fed
FelixKratz <felix.kratz@tu-dortmund.de>
2021-09-08 12:08:52 +0200

    fix wrong shasum

--- Casks/jupyterapp.rb
+++ Casks/jupyterapp.rb
@@ -4,7 +4,7 @@ cask "jupyterapp" do
   desc "A simple standalone macOS Jupyter App"
   homepage "https://github.com/FelixKratz/JupyterApp-mac"
   url "https://github.com/FelixKratz/JupyterApp-mac/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "103210d9ac97f0b5914c022c58d594417d354c05d221a7cf710a6db98ee9df69"
+  sha256 "d27582fcfce6242f2f78beeffbb70c39c983fc7661976d6111bf1efda53330d3"
 
   preflight do
     system "make"

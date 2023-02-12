292af120c0a3a8b8f21a5c330030d0321eef6a84
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-02-12 01:25:06 +0100

    update sketchybar formular to use service do

--- sketchybar.rb
+++ sketchybar.rb
@@ -44,42 +44,11 @@ class Sketchybar < Formula
     EOS
   end
 
-  plist_options manual: "sketchybar"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/sketchybar</string>
-        </array>
-        <key>EnvironmentVariables</key>
-        <dict>
-          <key>PATH</key>
-          <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-          <key>LANG</key>
-          <string>en_US.UTF-8</string>
-        </dict>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/sketchybar/sketchybar.out.log</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/sketchybar/sketchybar.err.log</string>
-        <key>ProcessType</key>
-        <string>Interactive</string>
-        <key>Nice</key>
-        <integer>-20</integer>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run "#{opt_bin}/sketchybar"
+    environment_variables PATH: std_service_path_env, LANG: "en_US.UTF-8"
+    keep_alive true
+    process_type :interactive
   end
 
   test do

9a1787ecb1090b4a093100831bafdcaf76ac2cb9
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-02-12 01:07:09 +0100

    testing services do

--- fyabai.rb
+++ fyabai.rb
@@ -27,39 +27,11 @@ class Fyabai < Formula
     bin.install "#{buildpath}/bin/yabai"
   end
 
-  plist_options :manual => "yabai"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/yabai</string>
-      </array>
-      <key>EnvironmentVariables</key>
-      <dict>
-        <key>PATH</key>
-        <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-      </dict>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/yabai/yabai.out.log</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/yabai/yabai.err.log</string>
-      <key>ProcessType</key>
-      <string>Interactive</string>
-      <key>Nice</key>
-      <integer>-20</integer>
-    </dict>
-    </plist>
-    EOS
+  service do
+    run "#{opt_bin}/yabai"
+    environment_variables PATH: std_service_path_env
+    keep_alive true
+    process_type :interactive
   end
 
   test do
